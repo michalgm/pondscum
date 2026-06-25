53e2a9b683ec3ecef184a7b8d53d8f6c7e06df76
Greg Michalec <greg+github@primate.net>
2026-06-24 18:17:06 -0700

    refactor generate.php * improved output handling * remove hardcoded song categories * better help * parallel processing * lint fixes

--- generate_charts/generate.php
+++ generate_charts/generate.php
@@ -1,95 +1,153 @@
 #!/usr/bin/php
 <?php
-include('../pondscum.php');
+include(__DIR__ . '/../pondscum.php');
 
-$lilydir = "../blo/";
-$output = "./output/";
-$dirs = array(
-	'current'=>'sheetmusic',
-	'oldcharts'=>'sheetmusic_archive',
-	'working'=>'sheetmusic_working'
-);
+$max_processes = 16;
+
 $lilies = array();
 
-if (isset($argv[1])) { 
-	if($argv[1] == 'all') { 
-		foreach(array_keys($dirs) as $dir) {
-			$outdir = $output.$dirs[$dir];
-			rrmdir("$outdir");
-			mkdir("$outdir");
+array_shift($argv);
+$output = preg_replace("/\/+$/", "", array_shift($argv));
 
-			$dirh = opendir($dir);
-			while (($file = readdir($dirh)) !== false) {
-				if ($file == 'include.ly' || ! preg_match("/\.ly$/", $file)) { continue; }
-				$lilies[] = processFile("$dir/$file");
-			}
-		}
-	} else {
-		array_shift($argv);
-		foreach($argv as $file) {
-			if ($file == 'include.ly' || ! preg_match("/\.ly$/", $file)) { continue; }
-			$lilies[] = processFile(preg_replace("/^\.+\//", "", $file));
-		}
-	}
-} else {
-	print "include lilypond files as arguments, or 'all' for all songs\n\n"; 
+if (! $output || $output == '--help' || $output == '-h') {
+	echo "Usage: generate.php <output_dir> [lilypond_files]\n";
 	exit;
 }
+if (! is_dir($output)) {
+	throw new Exception("Output directory '$output' invalid or does not exist");
+}
+
+echo "Output directory: $output\n";
+echo "Lilypond files: " . implode(", ", $argv) . "\n";
+
+foreach ($argv as $file) {
+	if ($file == 'include.ly' || ! preg_match("/\.ly$/", $file)) {
+		continue;
+	}
+	if (! file_exists($file)) {
+		throw new Exception("File '$file' does not exist");
+	}
+	$lilies[] = processFile(preg_replace("/^\.+\//", "", $file));
+}
+
 
 usort($lilies, 'lilysort');
 $index = "";
-foreach ($lilies as $lily) { 
+$pids = array();
+
+foreach ($lilies as $lily) {
+	processLily($lily);
+}
+
+function launch_job(array &$pids, callable $callback)
+{
+	global $max_processes;
+	global $status;
+	while (count($pids) >= $max_processes) {
+		$exited = pcntl_wait($status);
+		if ($exited > 0) {
+			unset($pids[$exited]);
+		}
+	}
+	$pid = pcntl_fork();
+	if ($pid == -1) {
+		print "could not fork\n";
+	} else if ($pid) {
+		$pids[$pid] = true;
+	} else {
+		ob_start();
+		$callback();
+		print ob_get_clean();
+		exit(0);
+	}
+}
+
+function processLily(array $lily)
+{
+	global $output, $keys, $clefs, $layouts, $musicdir;
+
+	$pids = array();
+
 	$musicdir = "";
-	if ($lily) { 
+	if ($lily) {
 		#set current date as tagline
 		$lily['source'] = preg_replace("/^\s+tagline ?=.*$/m", "", $lily['source']);
-		$lily['source'] = preg_replace("/\\\header\s*{/", "\\header { \n\ttagline = ".date('"n/j/Y"'), $lily['source']);
+		$lily['source'] = preg_replace("/\\\header\s*{/", "\\header { \n\ttagline = " . date('"n/j/Y"'), $lily['source']);
 
 		$title = str_replace(" ", "_", $lily['title']);
-		$pathinfo = pathinfo($lily['path']); 
-		$musicdir = $dirs[$pathinfo['dirname']];
-		if (! $musicdir) { die("Unknown directory $pathinfo[dirname]"); }
-		$musicdir = "$output/$musicdir/";
-		$dir = "$musicdir/".$title;
-		print getcwd()."\n";
+		$musicdir = "$output";
+		$dir = "$musicdir" . DIRECTORY_SEPARATOR . $title;
+		print getcwd() . "\n";
 		print "$dir\n";
 		if (is_dir($dir)) {
 			rrmdir($dir);
 		}
 		mkdir($dir);
-		print "$title: ";
+		print "$title: \n";
 		foreach (array('midi', 'score', 'source') as $part) {
-			print "\n$part\n ";
-			mkdir("$dir/$part");
+			print "$part\n ";
+			mkdir("$dir" . DIRECTORY_SEPARATOR . $part);
 			$lily['outputoptions']['key'] = "";
-			generateFile($lily, $title, $part);
+
+			launch_job($pids, function () use ($lily, $title, $part) {
+				// Use the modified $lily['file'] in child implicitly by modification
+				// But we need to ensure unique filenames if multiple processes run
+				$lily['file'] = str_replace('.ly', '', $lily['file']) . "_" . getmypid() . ".ly";
+				generateFile($lily, $title, $part);
+			});
 		}
+
 		if (isset($lily['description'])) {
 			file_put_contents("$dir/description.txt", $lily['description']);
 		}
+
 		foreach (array_keys($lily['parts']) as $part) {
 			$lily['outputoptions']['part'] = $part;
-			if ($part == 'words' || $part == 'lyrics') { 
+			$lily['outputoptions']['key'] = null;
+			$lily['outputoptions']['clef'] = null;
+
+			if ($part == 'words' || $part == 'lyrics') {
 				mkdir("$dir/$part");
-				generateFile($lily, $title, $part);
+				launch_job($pids, function () use ($lily, $title, $part) {
+					$lily['file'] = str_replace('.ly', '', $lily['file']) . "_" . getmypid() . ".ly";
+					generateFile($lily, $title, $part);
+				});
 			} else {
 				mkdir("$dir/$part");
 				foreach (array_keys($keys) as $key) {
 					$lily['outputoptions']['key'] = $key;
 					foreach ($clefs as $clef) {
 						$lily['outputoptions']['clef'] = $clef;
-						if ($clef == 'alto' || $clef == 'tenor') { continue; }
-						if ($clef == 'bass' && $key != 'C') { continue; }
+						if ($clef == 'alto' || $clef == 'tenor') {
+							continue;
+						}
+						// if ($clef == 'bass' && $instrument['key'] != 'C') {
+						// 	continue;
+						// }
 						foreach (array_keys($layouts) as $layout) {
 							$lily['outputoptions']['page'] = $layout;
 							$lily['outputoptions']['octave'] = 0;
-							$filename = generateFile($lily, $title, $part);
-							print "\t$filename\n";
+
+							launch_job($pids, function () use ($lily, $title, $part) {
+								$lily['file'] = str_replace('.ly', '', $lily['file']) . "_" . getmypid() . ".ly";
+								$filename = generateFile($lily, $title, $part);
+								print "\t$filename\n";
+							});
 						}
+						// }
 					}
 				}
 			}
 		}
+
+		// Wait for all parts to finish before zipping
+		while (count($pids) > 0) {
+			$exited = pcntl_wait($status);
+			if ($exited > 0) {
+				unset($pids[$exited]);
+			}
+		}
+
 		print "$title.zip\n";
 		chdir("$musicdir");
 		system("zip -qr \"$title/$title\".zip	\"$title\"");
@@ -98,9 +156,11 @@ foreach ($lilies as $lily) {
 	}
 }
 
-function generateFile($lily, $title, $part) {
+function generateFile(array $lily, string $title, string $part)
+{
 	global $musicdir;
-	$dir = "$musicdir/$title/$part/";
+
+	$dir = "$musicdir" . DIRECTORY_SEPARATOR . $title . DIRECTORY_SEPARATOR . $part;
 	$lily['outputoptions']['part'] = $part;
 	$lily = buildLayout($lily);
 	if ($part == 'midi') {
@@ -115,13 +175,14 @@ function generateFile($lily, $title, $part) {
 		}	
 	}
 
-	$filename = "$dir/$title.$ext";
+	$filename = $dir . DIRECTORY_SEPARATOR . $title . "." . $ext;
 	$output = createOutput($lily);
 	file_put_contents($filename, $output);
 	return $filename;
 }
 
-function rrmdir($path) {
+function rrmdir(string $path)
+{
 	return is_file($path)?
 		@unlink($path):
 		array_map('rrmdir',glob($path.'/*'))==@rmdir($path)
